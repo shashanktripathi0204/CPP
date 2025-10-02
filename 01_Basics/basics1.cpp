7f608470f45c858f3b547416fcdcb8d89540d6b0
shashanktripathi0204 <shashanktripathi99@outlook.com>
2025-10-03 01:36:47 +0530

    Getting started

---
+++ 01_Basics/basics1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+// Input
+
+int main() {
+    int num;
+    cout << "Enter the value of num:- ";
+    cin >> num;
+    cout << sizeof(num) <<endl;
+    cout << num <<endl;
+
+    char ch = 'k';
+    cout << ch << endl;
+    cout << sizeof(ch) << endl;
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CPP
+
+to run c++ in mac terminal use:-
+
+g++ file_name.cpp -o output_filename.cpp
+
+./<output_filename>
